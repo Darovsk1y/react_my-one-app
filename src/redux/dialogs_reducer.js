87d29881e31a938a1312c45ab3cd990f4411b66f
Darovsk1y <darovsk1y1@gmail.com>
2021-11-29 15:13:15 +0200

    if else replaced by switch case

--- src/redux/dialogs_reducer.js
+++ src/redux/dialogs_reducer.js
@@ -2,21 +2,23 @@ const ADD_MESSAGE = "ADD-MESSAGE";
 const TRACK_WRITE_MESSAGE = "TRACK-WRITE-MESSAGE";
 
 const dialogsReducer =(state, action)=>{
-	if(action.type === "ADD-MESSAGE"){
-		let newMessage = {
-			id: "5",
-			image: "http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg",
-			link: "#/",
-			position: "right",
-			message: state.newMessageText,
-		}
-		state.messages.push(newMessage);
-		state.newMessageText = '';
+	switch(action.type) {
+		case ADD_MESSAGE:
+			let newMessage = {
+				id: "5",
+				image: "http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg",
+				link: "#/",
+				position: "right",
+				message: state.newMessageText,
+			}
+			state.messages.push(newMessage);
+			state.newMessageText = '';
+			return state;
+		case TRACK_WRITE_MESSAGE:
+			state.newMessageText = action.message;
+			return state;
+		default:
+			return state;
 	}
-	else if(action.type === "TRACK-WRITE-MESSAGE"){
-		state.newMessageText = action.message;
-	}
-
-	return state;
 }
 export default dialogsReducer;
--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -4,21 +4,23 @@ const ADD_POST = "ADD-POST";
 const TRACK_WRITE_POST = "TRACK-WRITE-POST";
 
 const profileReducer =(state, action)=>{
-	if(action.type === "ADD-POST"){
-		let newPost = {
-			id: 4,
-			likes: 0,
-			name: "Raketa",
-			avatar: "https://scontent.fdnk5-1.fna.fbcdn.net/v/t1.6435-9/71788193_525812181517269_1225717343393415168_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Sk5ngR6CQa4AX-YK48H&tn=cowQwI4GI2N4ygpU&_nc_ht=scontent.fdnk5-1.fna&oh=1c6d0f524bfa3b80e7e77a8ab7c5b805&oe=61BCB1BD",
-			text: state.newPostText,
-		}
-		state.posts.push(newPost);
-		state.newPostText = '';
+	switch (action.type) {
+		case ADD_POST:
+			let newPost = {
+				id: 4,
+				likes: 0,
+				name: "Raketa",
+				avatar: "https://scontent.fdnk5-1.fna.fbcdn.net/v/t1.6435-9/71788193_525812181517269_1225717343393415168_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=Sk5ngR6CQa4AX-YK48H&tn=cowQwI4GI2N4ygpU&_nc_ht=scontent.fdnk5-1.fna&oh=1c6d0f524bfa3b80e7e77a8ab7c5b805&oe=61BCB1BD",
+				text: state.newPostText,
+			}
+			state.posts.push(newPost);
+			state.newPostText = '';
+			return state;
+		case TRACK_WRITE_POST:
+			state.newPostText = action.text;
+			return state;
+		default:
+			return state;
 	}
-	else if(action.type === "TRACK-WRITE-POST"){
-		state.newPostText = action.text;
-	}
-
-	return state;
 }
 export default profileReducer;
