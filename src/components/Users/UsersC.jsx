e25a0398905c23120dd1158efbe8270ef6ec3ec2
Darovsk1y <darovsk1y1@gmail.com>
2021-12-06 21:03:37 +0200

    axios moved into component-did-mount

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -4,8 +4,7 @@ import React from "react";
 const axios = require("axios");
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount(){
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
@@ -13,36 +12,12 @@ class Users extends React.Component {
       });
   }
 
-  /*   userElements = () =>{
-    this.props.users.map((u) => {
-      return (
-        <User
-          key={u.id}
-          id={u.id}
-          name={u.name}
-          status={u.status}
-          adress={"u.adress"}
-          avatar={
-            u.photos.large != null
-              ? u.photos
-              : "https://ava-24.com/_ph/98/563228947.jpg"
-          }
-          isfollow={u.followed}
-          link={u.link != null ? u.link : "#/"}
-          follow={this.props.follow}
-          unfollow={this.props.unfollow}
-          setUser={this.props.setUser}
-        />
-      );
-    });
-} */
-  /*! Не удальсь вынести map в отдельную ф-ию, ругается что --> Функции недействительны в качестве дочерних React  */
   render() {
     return (
       <div className={s.users}>
         <div className={s.body}>
           <ul className={s.userList}>
-            {/* поэтому вставили кодом без отдельного вызова и убрав return */}
+            {/* ругался на ф-ию поэтому вставили кодом без отдельного вызова и убрав return */}
             {this.props.users.map((u) => (
               <User
                 key={u.id}
